903d98f34da53cc12fcd31b90b13f73754d69546
Frank_Paz <81579262+1drakr@users.noreply.github.com>
2024-07-13 14:24:05 -0500

    Update create.blade.php

--- resources/views/evaluarproyecto/create.blade.php
+++ resources/views/evaluarproyecto/create.blade.php
@@ -96,7 +96,7 @@
                             <h3 class="card-title">Documento del Proyecto</h3>
                         </div>
                         <div class="card-body">
-                            <a href="{{ Storage::url($pdfPath) }}" class="btn btn-info" download>
+                            <a href="{{ asset('storage/' . $pdfPath) }}" class="btn btn-info" download>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
                                      viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
                                      stroke-linecap="round" stroke-linejoin="round">
